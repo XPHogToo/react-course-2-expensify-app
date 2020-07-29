8fc6db75e5c62c554663a71bc43a0d5bb5bca517
Cummings-Kralik <Ronald.Cummings-Kralik@bausch.com>
2020-07-29 14:35:31 -0500

    Added setExpenses on load & test

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -25,43 +25,52 @@ test('Should not remove expense if id not found', () => {
 });
 
 test('Should add an expense', () => {
-  const expense = {
-    id: '109',
-    description: 'Laptop',
-    note: '',
-    createdAt: 20000,
-    amount: 29500
-  };
-  const action = {
-    type: 'ADD_EXPENSE',
-    expense
-  };
-  const state = expensesReducer(expenses, action);
-  expect(state).toEqual([...expenses, expense]);
+	const expense = {
+		id: '109',
+		description: 'Laptop',
+		note: '',
+		createdAt: 20000,
+		amount: 29500
+	};
+	const action = {
+		type: 'ADD_EXPENSE',
+		expense
+	};
+	const state = expensesReducer(expenses, action);
+	expect(state).toEqual([...expenses, expense]);
 });
 
 test('Should edit an expense', () => {
-  const amount = 122000;
-  const action = {
-    type: 'EDIT_EXPENSE',
-    id: expenses[1].id,
-    updates: {
-      amount
-    }
-  };
-  const state = expensesReducer(expenses, action);
-  expect(state[1].amount).toBe(amount);
+	const amount = 122000;
+	const action = {
+		type: 'EDIT_EXPENSE',
+		id: expenses[1].id,
+		updates: {
+			amount
+		}
+	};
+	const state = expensesReducer(expenses, action);
+	expect(state[1].amount).toBe(amount);
 });
 
 test('Should fail to edit an expense if the id is not found', () => {
-  const amount = 122000;
-  const action = {
-    type: 'EDIT_EXPENSE',
-    id: '-1',
-    updates: {
-      amount
-    }
+	const amount = 122000;
+	const action = {
+		type: 'EDIT_EXPENSE',
+		id: '-1',
+		updates: {
+			amount
+		}
 	};
 	const state = expensesReducer(expenses, action);
 	expect(state).toEqual(expenses);
+});
+
+test('Should set expenses', () => {
+	const action  = {
+		type: 'SET_EXPENSES',
+		expenses: [expenses[1]]
+	};
+	const state = expensesReducer(expenses, action);
+	expect(state).toEqual([expenses[1]]);
 });
